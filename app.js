199d2e7aa42c710b0fbec81abfe946961ce8817a
Slavka 47 <pasportnikov2000@gmail.com>
2017-10-25 15:05:22 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require( "express" );
+const Web3 = require( "web3" );
+const contract = require( "./contract" );
+
 const app = express();
+const web3 = new Web3();
+
+web3.setProvider( new web3.providers.HttpProvider( "http://localhost:8545" ) );
+const helloContract = web3.eth.contract( contract.ABI ).at( contract.address );
 
 app.get( "/", function( req, res ){
-  res.send( "Hello" );
+  web3.personal.unlockAccount( web3.eth.coinbase, "ytpyf..c1", 1000 );
+  
+  res.send( helloContract.setData.sendTransaction( "Hell", { from: web3.eth.coinbase } ) );
 } );
 
 app.listen( 8040, function(){
